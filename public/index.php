8b41f1e2fd5e14d73b8b9b029f12cc5bc7d01587
pseudoanime <luckilaxmi@gmail.com>
2017-12-17 02:52:51 +0000

    add database stuff

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
 if (PHP_SAPI == 'cli-server') {
@@ -30,12 +29,5 @@ require __DIR__ . '/../src/middleware.php';
 // Register routes
 require __DIR__ . '/../src/routes.php';
 
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $name = $request->getAttribute('name');
-    $response->getBody()->write("Hello, $name");
-    $this->logger->addInfo('Something interesting happened');
-
-    return $response;
-});
 // Run app
 $app->run();
--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,26 @@ use Slim\Http\Response;
 
 // Routes
 
+$app->get('/tickets', function (Request $request, Response $response) {
+    $this->logger->addInfo('Ticket list');
+    $mapper = new TicketMapper($this->db);
+    $tickets = $mapper->getTickets();
+
+    $response->getBody()->write(var_export($tickets, true));
+
+    return $response;
+});
+
+$app->get('/ticket/{id}', function (Request $request, Response $response, $args) {
+    $ticket_id = (int) $args['id'];
+    $mapper = new TicketMapper($this->db);
+    $ticket = $mapper->getTicketById($ticket_id);
+
+    $response->getBody()->write(var_export($ticket, true));
+
+    return $response;
+});
+
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     // Sample log message
     $this->logger->info("Slim-Skeleton '/' route");
